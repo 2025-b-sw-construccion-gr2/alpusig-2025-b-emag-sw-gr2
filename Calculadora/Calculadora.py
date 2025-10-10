3c69fb34d32fcd54a805fb38c870d0a85ecfdef1
Erick <erick.alpusig@epn.edu.ec>
2025-10-10 15:36:37 -0500

    UPDATE

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,7 +1,17 @@
+from Suma.Suma import Suma
+from resta.Resta import Resta
+
 def main():
     print ("Bienvenido a la calculadora EMAG")
-    resultado_suma = Suma.sumar(7,7)
-    resultado_resta = Resta.restar(10,5)
+
+    numero1 = float (input ("Digite el numero uno: "))
+    numero2 = float (input ("Digite el numero dos: "))
+
+    suma_obj = Suma(numero1, numero2)
+    resta_obj = Resta(numero1, numero2)
+
+    suma_obj.sumar()    
+    resta_obj.restar()
 
 if __name__ == "__main__":
     main()
